b9b15cb164fabc0a7520853fc4f4afcadd38a7c8
Muslovi <musloviplay@gmail.com>
2025-01-08 20:52:07 +0100

    Upd

--- scripts/openLedger/resetPorts.sh
+++ scripts/openLedger/resetPorts.sh
@@ -11,6 +11,16 @@ SCRAPER_PORT=8000
 COMPOSE_FILE_PATH="/root/.config/opl/docker-compose.yaml"
 CONGIG_FILE_PATH="/root/.config/opl/config.yaml"
 
+# Функция проверки, занят ли порт
+is_port_in_use() {
+  local port=$1
+  if ss -tuln | grep -q ":$port "; then
+    return 0  # Порт занят
+  else
+    return 1  # Порт свободен
+  fi
+}
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --port1) WORKER_PORT_1=$2; shift ;;
@@ -21,17 +31,37 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
+
+# Проверяем, существует ли файл
+if [[ ! -f "$COMPOSE_FILE_PATH" ]]; then
+  echo "Файл $COMPOSE_FILE_PATH не найден!"
+  exit 1
+fi
+
+if [[ ! -f "$CONGIG_FILE_PATH" ]]; then
+  echo "Файл $CONGIG_FILE_PATH не найден!"
+  exit 1
+fi
+
+# Проверяем все порты
+for PORT in $WORKER_PORT_1 $WORKER_PORT_1 $WORKER_PORT_1; do
+  if is_port_in_use $PORT; then
+    echo "Ошибка: порт $PORT уже занят!"
+    exit 1
+  fi
+done
+
 # Замена портов в файле docker-compose.yaml
 sed -i \
-  -e "s/8080:8080/${WORKER_PORT1}:${WORKER_PORT1}/g" \
-  -e "s/5555:5555/${WORKER_PORT2}:${WORKER_PORT2}/g" \
+  -e "s/8080:8080/${WORKER_PORT_1}:${WORKER_PORT_1}/g" \
+  -e "s/5555:5555/${WORKER_PORT_2}:${WORKER_PORT_2}/g" \
   -e "s/8000:8000/${SCRAPER_PORT}:${SCRAPER_PORT}/g" \
   "$COMPOSE_FILE_PATH"
 
 # Замена портов в файле docker-compose.yaml
 sed -i \
-  -e "s/8080/${WORKER_PORT1}:${WORKER_PORT1}/g" \
-  -e "s/5555/${WORKER_PORT2}:${WORKER_PORT2}/g" \
+  -e "s/8080/${WORKER_PORT_1}:${WORKER_PORT_1}/g" \
+  -e "s/5555/${WORKER_PORT_2}:${WORKER_PORT_2}/g" \
   -e "s/8000/${SCRAPER_PORT}:${SCRAPER_PORT}/g" \
   "$CONGIG_FILE_PATH"
 
